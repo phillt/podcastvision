66181a75b0650d12d65b90821fc2aeea901661f4
Philip <powerphillg5@gmail.com>
2020-05-24 17:33:41 -0700

    add build

---
+++ dist/W8ASnyzZ534.js
@@ -0,0 +1,55 @@
+export const s = {
+    "0, 25": [
+        {
+            component_name: "PresentTwitterCard",
+            props : {
+                description: "Joseph James Rogan is an American comedian and podcast host. He has also worked as a mixed martial arts color commentator, television host, and occasional actor.",
+                handle:"@joerogan",
+                img:"https://i.ytimg.com/vi/ESYYby1FuPc/maxresdefault.jpg",
+                link:"https://twitter.com/joerogan?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+                name: "Joe Rogan",
+            }
+        },
+    ],
+
+    "5,20": [
+        {
+            component_name: "PresentTwitterCard",
+            props : {
+                description: "Greg Fitzsimmons is a stand-up comedian, writer, producer, and host of Fitzdog Radio on Howard Stern's SiriusXM and his own twice-weekly podcast.",
+                handle:"@GregFitzShow",
+                img:"https://s3-us-east-2.amazonaws.com/redefined/wp-content/uploads/2019/03/14102521/GregFitzsimmons_ViaArtist.jpg",
+                link:"https://twitter.com/GregFitzShow?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+                name: "Creg Fitzsimmons",
+            }
+        }
+    ],
+    "110,150": [
+        {
+            component_name: "PresentGenericCard",
+            props: {
+                img: "https://www.centralpark.com/downloads/4809/download/wollman-rink.jpe?cb=783665e2e86dabe11fdbe675f24e26c4&w=1800",
+                links: [
+                    {
+                        href: "https://www.google.com/search?q=wollman+rink&tbm=isch&ved=2ahUKEwjNnbKy2dLoAhVDmp4KHXp4ACAQ2-cCegQIABAA&oq=wolman+rink&gs_lcp=CgNpbWcQARgAMgYIABAKEBgyBggAEAoQGDIGCAAQChAYMgYIABAKEBg6BAgjECc6AggAOgQIABBDOgYIABAIEB46BAgAEB46BAgAEBhQqDtY91Zg2mFoAHAAeACAAUuIAe0EkgECMTGYAQCgAQGqAQtnd3Mtd2l6LWltZw&sclient=img&ei=foyKXs3mO8O0-gT68IGAAg&bih=949&biw=1920#imgrc=SnVO6OdUzV4vlM",
+                        name: "Woolman Rink in Central Park"
+                    }
+                ]
+            }
+        }
+    ],
+    "350, 380": [
+        {
+            component_name: "PresentGenericCard",
+            props: {
+                img: "https://vetstreet.brightspotcdn.com/dims4/default/94eaf65/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F72%2F32%2Ff2a109cc4a59aec759fca90db449%2Fmastiff-ap-1czd1n-645.jpg",
+                links: [
+                    {
+                        href: "https://www.google.com/search?q=mastiff&sxsrf=ALeKk00bW5Sb0dyZEZIsAD66BXNyk9Crww:1586660484369&source=lnms&tbm=isch&sa=X&ved=2ahUKEwic5rL08uHoAhULGTQIHeuuAL0Q_AUoAXoECB4QAw&biw=1375&bih=818#imgrc=sbdCnUTfFrmgTM",
+                        name: "Mastiff",
+                    }
+                ],
+            }
+        }
+    ]
+};
